bebfe451c521cbfea07f721d05417bd22f49e009
sophie-gill <164692272+sophie-gill@users.noreply.github.com>
2024-04-12 16:21:52 -0500

    added volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,8 @@
+def calculate_volume_of_sphere(radius):
+    import math
+    volume = (4/3)* math.pi * (radius**3)
+    return volume
+
+calculate_volume_of_sphere(30)
+
+calculate_volume_of_sphere(40)
